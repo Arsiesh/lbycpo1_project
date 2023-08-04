ed1a94bf91536f101b0a79120cff35e0eae8b1fc
Arsiesh <129363666+Arsiesh@users.noreply.github.com>
2023-08-04 17:14:54 +0800

    queue

--- number_2/cpo1shell.sh
+++ number_2/cpo1shell.sh
@@ -18,6 +18,39 @@ run_c_code() {
     fi
 }
 
+# Array to keep track of running and queued programs
+declare -a running_programs=()
+
+# Background function to handle dispatching and queuing
+dispatcher() {
+    while true; do
+        # Check if there are running programs
+        if [ ${#running_programs[@]} -lt 3 ]; then
+            # If there are queued programs, start the one with the smallest physical size
+            if [ ${#queued_programs[@]} -gt 0 ]; then
+                smallest_index=0
+                smallest_size=${queued_programs[0]}
+                for (( i=0; i<${#queued_programs[@]}; i++ )); do
+                    if [ "${queued_programs[i]}" -lt "$smallest_size" ]; then
+                        smallest_size=${queued_programs[i]}
+                        smallest_index=$i
+                    fi
+                done
+
+                program_to_run="${queued_programs[smallest_index]}"
+                unset 'queued_programs[smallest_index]'
+                running_programs+=("$program_to_run")
+                run_c_code "$program_to_run" &
+
+            fi
+        fi
+        sleep 1 # Adjust the sleep interval if needed
+    done
+}
+
+# Start the dispatcher function in the background
+dispatcher &
+
 # Main loop
 while true; do
     prompt
@@ -28,13 +61,29 @@ while true; do
         *\&*)
             input="${input#& }" # Remove the "&" and space from the input
             if [[ "$input" == *.c ]]; then
-                run_c_code "$input" &
+                if [ ${#running_programs[@]} -lt 3 ]; then
+                    # If there are fewer than 3 running programs, start the program immediately
+                    running_programs+=("$input")
+                    run_c_code "$input" &
+                else
+                    # Otherwise, queue the program
+                    queued_programs+=($(stat -c %s "$input")) # Store the physical size of the program
+                    echo "Program $input queued."
+                fi
             else
                 eval "$input &" # Run other commands in the background
             fi
             ;;
         *.c)
-            run_c_code "$input"
+            if [ ${#running_programs[@]} -lt 3 ]; then
+                # If there are fewer than 3 running programs, start the program immediately
+                running_programs+=("$input")
+                run_c_code "$input" &
+            else
+                # Otherwise, queue the program
+                queued_programs+=($(stat -c %s "$input")) # Store the physical size of the program
+                echo "Program $input queued."
+            fi
             ;;
         exit)
             echo "Goodbye!"
@@ -45,3 +94,4 @@ while true; do
             ;;
     esac
 done
+
